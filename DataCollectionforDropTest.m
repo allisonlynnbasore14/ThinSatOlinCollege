7a3a1bfdc9700e2461c954d4631dff44b2be81f6
Allison Basore <allison.basore@students.olin.edu>
2018-07-18 13:38:22 -0400

    Updated Matlab plotting file

--- DataCollectionforDropTest.m
+++ DataCollectionforDropTest.m
@@ -1,9 +1,14 @@
+% Takes accel, gyro, and mag data from LSM9DS1 Ardunio sensor via an SD
+% and plots the x,y,and z values of each.
+
+% Replace DATAVALS.TXT with the file from the SD card and run this file
+
 fileName = 'DATAVALS.TXT';
 pivot = 3; % number of items: accel, mag, gyro
 A = importdata(fileName,' ');
 A = A.textdata;
 A = cell2table(A);
-A = table2array(A)
+A = table2array(A);
 time = zeros(round(size(A,1)/pivot),1);
 
 dataVals = zeros(size(A));
@@ -32,63 +37,3 @@ figure
 plot(time,dataVals(:,14),time, dataVals(:,16),time, dataVals(:,18))
 title('Gyro Data')
 legend('x','y','z')
-
-
-
-
-% 
-% accel = zeros(round(size(A,1)/pivot),3); % in x,y,z order
-% mag = zeros(round(size(A,1)/pivot),3); % Magnetometer in x,y,z order
-% gyro = zeros(round(size(A,1)/pivot),3); % in x,y,z order
-% time = zeros(round(size(A,1)/pivot),1);
-% allData = [accel,mag,gyro,time];
-% 
-% typeCount = 1;
-% aIndex= 1;
-% mIndex=1;
-% gIndex=1;
-% test = getXYZvals(A(1,:))
-% for i = 1:size(A,1)
-%     if(typeCount == 1)
-% %        for j = 1:size(A,2)
-% %            %accel(aIndex,j) = A(i,j);
-% %        end
-%        [Ax,Ay,Az,Mx,My,Mz,Gx,Gy,Gz] = getXYZvals(A(i,:));
-%        accel(aIndex,1) = x;
-%        accel(aIndex,2) = y;
-%        accel(aIndex,3) = z;
-%        time(aIndex,1) = aIndex;
-%        aIndex = aIndex +1;
-%     end
-%     if(typeCount == 2)
-%        for j = 1:size(A,2)
-%            %mag(mIndex,j) = A(i,j);
-%        end
-%        mIndex = mIndex +1;
-%     end
-%     if(typeCount == 3)
-%        for j = 1:size(A,2)
-%            %gyro(gIndex,j) = A(i,j);
-%        end
-%        gIndex = gIndex +1;
-%     end
-%     typeCount = typeCount +1;
-%     if(typeCount > 3)
-%         typeCount = 1;
-%     end
-%    
-% end
-% 
-% function out = getXYZvals(input)
-%     count = 1;
-%     outArray = zeros(1,9);
-%     for j = 1:2:size(input)
-%         outArray(count) = input(j);
-%         count = count + 1;
-%     end
-%     out = outArray;
-% end
-% 
-% 
-
-% 
